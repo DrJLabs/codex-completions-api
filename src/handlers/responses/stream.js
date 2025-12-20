3adf1db7e40f0c8256008358eaa15c4c6ef5fac4
DrJsPBs <drj@drjlabs.com>
2025-12-20 11:48:48 -0500

    fix(responses): default copilot output mode to obsidian-xml

--- src/handlers/responses/stream.js
+++ src/handlers/responses/stream.js
@@ -1,5 +1,9 @@
 import { postChatStream } from "../chat/stream.js";
-import { applyDefaultProxyOutputModeHeader, coerceInputToChatMessages } from "./shared.js";
+import {
+  applyDefaultProxyOutputModeHeader,
+  coerceInputToChatMessages,
+  resolveResponsesOutputMode,
+} from "./shared.js";
 import { createResponsesStreamAdapter } from "./stream-adapter.js";
 import { config as CFG } from "../../config/index.js";
 import { logResponsesIngressRaw } from "./ingress-logging.js";
@@ -32,10 +36,12 @@ export async function postResponsesStream(req, res) {
   const outputModeRequested = req.headers["x-proxy-output-mode"]
     ? String(req.headers["x-proxy-output-mode"])
     : null;
-  const restoreOutputMode = applyDefaultProxyOutputModeHeader(req, CFG.PROXY_RESPONSES_OUTPUT_MODE);
-  const outputModeEffective = req.headers["x-proxy-output-mode"]
-    ? String(req.headers["x-proxy-output-mode"])
-    : null;
+  const { effective: outputModeEffective } = resolveResponsesOutputMode({
+    req,
+    defaultValue: CFG.PROXY_RESPONSES_OUTPUT_MODE,
+    copilotDefault: "obsidian-xml",
+  });
+  const restoreOutputMode = applyDefaultProxyOutputModeHeader(req, outputModeEffective);
   locals.output_mode_requested = outputModeRequested;
   locals.output_mode_effective = outputModeEffective;
 
